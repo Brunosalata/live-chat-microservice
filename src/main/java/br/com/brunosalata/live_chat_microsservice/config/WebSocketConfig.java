9b67687155bbca436037aa5bd9347eadf60c6891
Bruno Salata <brunoslima@gmail.com>
2025-03-16 19:13:00 -0300

    WebSocket Configuration

---
+++ src/main/java/br/com/brunosalata/live_chat_microsservice/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package br.com.brunosalata.live_chat_microsservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author Bruno Salata Lima
+ * github.com/Brunosalata
+ */
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topics");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/live-chat");
+    }
+}
