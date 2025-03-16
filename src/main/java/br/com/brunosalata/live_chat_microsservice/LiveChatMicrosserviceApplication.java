ac38581d2c41a5a55271b2fbfbe5ca4b9f58e5ce
Bruno Salata <brunoslima@gmail.com>
2025-03-16 19:01:27 -0300

    First commit

---
+++ src/main/java/br/com/brunosalata/live_chat_microsservice/LiveChatMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunosalata.live_chat_microsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunosalata/live_chat_microsservice/LiveChatMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunosalata.live_chat_microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
